cd94241089425dae3b4c1cf2a0597d45d8923b6f
Vadym <Sum.vadim415579suq@gmail.com>
2024-03-02 18:29:33 +0200

    Rewrite the code into one function

--- app/src/main/java/ua/edu/lntu/cw4/MainActivity.kt
+++ app/src/main/java/ua/edu/lntu/cw4/MainActivity.kt
@@ -1,40 +1,48 @@
 package ua.edu.lntu.cw4
 
-
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.NavType
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
+import androidx.navigation.compose.*
 import androidx.navigation.navArgument
 import ua.edu.lntu.cw4.ui.theme.IPZ_CR_4Theme
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            IPZ_CR_4Theme {
-                val navController = rememberNavController()
-                NavHost(navController, startDestination = "first") {
-                    composable("first") { FirstScreen(navController) }
-                    composable("second/{selectedItem}") { backStackEntry ->
-                        SecondScreen(
-                            selectedItem = backStackEntry.arguments?.getInt("selectedItem") ?: -1,
-                            navController = navController
-                        )
-                    }
+            AppMain()
+        }
+    }
+}
+
+@Composable
+fun AppMain() {
+    IPZ_CR_4Theme {
+        val navController = rememberNavController()
+        NavHost(navController, startDestination = "first") {
+            composable("first") { FirstScreen(navController) }
+            composable("second/{selectedItem}", arguments = listOf(navArgument("selectedItem") { type = NavType.IntType })) { backStackEntry ->
+                val selectedItem = backStackEntry.arguments?.getInt("selectedItem")
+                if (selectedItem != null) {
+                    SecondScreen(selectedItem, navController)
+                } else {
+                    // Handle error case
                 }
             }
         }
@@ -44,34 +52,63 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun FirstScreen(navController: NavController) {
     Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 34.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+        verticalArrangement = Arrangement.Top
     ) {
-        Text(text = "Список справ")
+        Text(
+            text = "List elements",
+            fontSize = 21.sp,
+            fontWeight = FontWeight.Bold
+        )
         ItemList(navController)
     }
 }
 
 @Composable
 fun ItemList(navController: NavController) {
-    val items = listOf("Зробити виріб", "Прибратись", "Лягти спати")
-    Column {
+    val items = listOf("First element", "Second element", "Third element")
+    Column(Modifier.padding(top = 15.dp)) {
         items.forEachIndexed { index, item ->
             Text(
                 text = item,
                 modifier = Modifier
-                    .clickable { navController.navigate("second/$index") }
+                    .clickable { navController.navigate("second/${index + 1}") }
                     .padding(top = 12.dp)
             )
         }
     }
 }
 
+@Composable
+fun SecondScreen(selectedItem: Int, navController: NavController) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 34.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top
+    ) {
+        Text(
+            text = "About element",
+            fontSize = 21.sp,
+            fontWeight = FontWeight.Bold
+        )
+        Text(text = "Number Of Element: $selectedItem")
+        Button(
+            onClick = { navController.navigateUp() },
+            modifier = Modifier.padding(top = 11.dp)) {
+            Text("Back")
+        }
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+fun GreetingPreview() {
     IPZ_CR_4Theme {
-        MyApp()
+        AppMain()
     }
 }
